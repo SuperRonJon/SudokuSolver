ff1e6dae423834b48b14e54b698f6b8182a4d434
Patrick Seute <patrick.seute@cox.net>
2025-02-24 10:13:32 -0500

    move board solving to function

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -5,6 +5,7 @@
 #include "Board.h"
 #include <string>
 
+void solveBoard(std::string boardString, bool borders);
 
 int main(int argc, char* argv[])
 {
@@ -14,16 +15,20 @@ int main(int argc, char* argv[])
         return -1;
     }
     std::string boardString = argv[1];
+    solveBoard(boardString, true);
+}
+
+void solveBoard(std::string boardString, bool borders) {
     if (boardString.size() != 81) {
-        std::cout << "Invalid arguments. String is not a valid sudoku board length. Must be 81 characters." << std::endl;
-        return -1;
+        std::cout << "Invalid board string, incorrect length, must be 81 characters" << std::endl;
+        return;
     }
+
     Board board(boardString);
-    board.print(true);
-    bool solved = board.solve();
-    if (solved) {
+    board.print(borders);
+    if (board.solve()) {
         std::cout << "Solved!" << std::endl;
-        board.print(true);
+        board.print(borders);
     }
     else {
         std::cout << "Unable to solve..." << std::endl;
