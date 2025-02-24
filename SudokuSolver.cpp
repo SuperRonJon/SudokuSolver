e7decf94474589f2cac6d56e61f616206ef09375
Patrick Seute <patrick.seute@cox.net>
2025-02-24 11:38:01 -0500

    add example board back to failed outputs

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -10,14 +10,21 @@ void solveBoard(std::string boardString, bool borders);
 
 int main(int argc, char* argv[])
 {
+    std::string exampleBoard = "x56xxx27872xx361xx8xxxxx46x5xxx47xxx4x9xxx7x5xxx65xxx4x35xxxxx7xx718xx32918xxx54x";
     InputParser parser(argc, argv);
     
     bool bordersOn = parser.getBordersOn();
     std::string boardString = parser.getBoardString();
+    if (boardString == "example") {
+        std::cout << "Example: " << exampleBoard << std::endl;
+        return 0;
+    }
     if (boardString != "") {
         solveBoard(boardString, bordersOn);
         return 0;
     }
+    std::cout << "No board string detected. Must be 81 character string with one of x,X,0,o,O,. as empty characers." << std::endl;
+    std::cout << "Example: " << exampleBoard << std::endl;
     return -1;
     
 }
